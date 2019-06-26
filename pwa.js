2a6636022f9a80aa8692258364ec4e16e619a576
SutiknoGo <37078203+SutiknoGo@users.noreply.github.com>
2019-06-26 09:49:02 +0700

    update pwa.js and sw.js

--- pwa.js
+++ pwa.js
@@ -1,42 +1,39 @@
 var deferredPrompt;
-var btnAdd = document.querySelector('#download-pwa');
+  var btnAdd = document.querySelector('#download-pwa');
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  console.log('deferredPrompt!');
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can add to home screen
-  btnAdd.style.display = 'block';
-});
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js')
-      .then((reg) => {
-        console.log('Service worker registered. scope: ', reg.scope);
-      }, function (err) {
-        console.log('ServiceWorker registration failed: ', err);
-      });
+  window.addEventListener('beforeinstallprompt', (e) => {
+    console.log('deferredPrompt!');
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can add to home screen
+    btnAdd.style.display = 'block';
   });
-}
 
-
-// Installation must be done by a user gesture! Here, the button click
-btnAdd.addEventListener('click', (e) => {
-  // hide our user interface that shows our A2HS button
-  btnAdd.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice
-    .then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('/sw.js')
+    .then(function() {
+      console.log('Service worker registered!');
     });
-});
+  }
+
+
+    // Installation must be done by a user gesture! Here, the button click
+    btnAdd.addEventListener('click', (e) => {
+      // hide our user interface that shows our A2HS button
+      btnAdd.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+    });
--- sw.js
+++ sw.js
@@ -1,82 +1,82 @@
 var CACHE_STATIC_NAME = "static-test";
 var CACHE_DYNAMIC_NAME = "dynamic-test";
 
-const STATIC_ASSETS = [
-  './',
-  './offline.html',
-  './js/jqm-demos.js',
-  './js/jquery.js',
-  './js/jquery.mobile-1.4.5.js',
-  './js/jquery.mobile-1.4.5.min.js',
-  './js/jquery.mobile-1.4.5.min.map',
-  './js/pwa.js',
-  './js/view-source.js',
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME)
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.addAll([
+          '/',
+          '/offline.html',
+          '/js/jqm-demos.js',
+          '/js/jquery.js',
+          '/js/jquery.mobile-1.4.5.js',
+          '/js/jquery.mobile-1.4.5.min.js',
+          '/js/jquery.mobile-1.4.5.min.map',
+          '/js/pwa.js',
+          '/js/view-source.js',
 
-  './css/jquery.mobile-1.4.5.min.css',
-  './css/jqm-demos.css',
-  './css/style.css',
-  './css/responsive.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
-  'https://fonts.googleapis.com/css?family=Open+Sans:300,400,700',
+          '/css/jquery.mobile-1.4.5.min.css',
+          '/css/jqm-demos.css',
+          '/css/style.css',
+          '/css/responsive.css',
+          'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+          'http://fonts.googleapis.com/css?family=Open+Sans:300,400,700',
 
-  './images/social/dribbble.png',
-  './images/social/facebook.png',
-  './images/social/google.png',
-  './images/social/pinterest.png',
-  './images/social/twitter.png',
-  './images/favicon.ico',
-  './images/apple-touch-icon.png',
-  './images/logoW.png'
-];
-
-const STATIC_CACHE_NAME = 'static-test';
-const DYNAMIC_CACHE_NAME = 'dynamic-test';
-
-self.addEventListener('install', async evt => {
-  const cache = await caches.open(STATIC_CACHE_NAME);
-  cache.addAll(STATIC_ASSETS);
+          '/images/social/dribbble.png',
+          '/images/social/facebook.png',
+          '/images/social/google.png',
+          '/images/social/pinterest.png',
+          '/images/social/twitter.png',
+          '/images/favicon.ico',
+          '/images/apple-touch-icon.png',
+          '/images/logoW.png'
+        ]);
+      })
+  )
 });
 
-self.addEventListener('fetch', evt => {
-  const req = evt.request;
-  const url = new URL(req.url);
-
-  // check where to fetch. from our own cache or network.
-  if (url.origin == location.origin) {
-    evt.respondWith(cacheFirst(req));
-  } else {
-    evt.respondWith(networkFirst(req));
-  }
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
 });
 
-async function cacheFirst(req) {
-  const cachedResponse = await caches.match(req);
-  return cachedResponse || fetch(req);
-}
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch(function(err) {
+              return caches.open(CACHE_STATIC_NAME)
+                .then(function(cache) {
+                  return cache.match('/offline.html');
+                });
+            });
 
-async function networkFirst(req) {
-  const cache = await caches.open(DYNAMIC_CACHE_NAME);
-  try {
-    // try go to network and fetch data 
-    const res = await fetch(req);
-    cache.put(req, res.clone());
-    return res;
-  } catch (error) {
-    // look something on cache. 
-    const cachedResponse = await cache.match(req);
-    return cachedResponse;
-  }
-}
-
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== STATIC_CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
         }
-      }));
-    })
+      })
   );
 });
