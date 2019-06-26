4992cfdcdbb1f18057d9edd7396c94c8b49741a5
SutiknoGo <37078203+SutiknoGo@users.noreply.github.com>
2019-06-26 10:01:13 +0700

    update .html

--- pwa.js
+++ pwa.js
@@ -13,7 +13,7 @@ var deferredPrompt;
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-    .register('/sw.js')
+    .register('sw.js')
     .then(function() {
       console.log('Service worker registered!');
     });
